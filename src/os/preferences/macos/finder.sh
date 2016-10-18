410868aecd254f0b735086eafdb8525e9efe906e
Steve Daly <steve@thriveplan.com>
2016-10-18 10:39:00 -0400

    Do not open finder window on mount. Update VIM plugins

--- src/os/preferences/macos/finder.sh
+++ src/os/preferences/macos/finder.sh
@@ -7,10 +7,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Finder\n\n"
 
-execute "defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true && \
-         defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true && \
-         defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true" \
-    "Automatically open a new Finder window when a volume is mounted"
+execute "defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool false && \
+         defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool false && \
+         defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool false" \
+         "Do not open a new Finder window when a volume is mounted"
 
 execute "defaults write com.apple.finder _FXShowPosixPathInTitle -bool true" \
     "Use full POSIX path as window title"
