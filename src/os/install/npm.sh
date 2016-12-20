ccd720a08d08ba01fe07127382f07a509419bb2f
Steve Daly <steve@thriveplan.com>
2016-12-20 10:24:19 -0500

    Subproject Updates

--- src/os/install/npm.sh
+++ src/os/install/npm.sh
@@ -46,6 +46,7 @@ main() {
     install_npm_package "Firebase Tools" "firebase-tools"
     install_npm_package "Fabulous Kill" "fkill-cli"
     install_npm_package "Express Generator" "express-generator"
+    install_npm_package "Yarn"  "yarn"
 }
 
 main
